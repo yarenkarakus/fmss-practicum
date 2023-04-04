3a0b7326cb0874389be78d05dd000d1e0b3a0285
Yaren Karakus <yarenkarakus007@gmail.com>
2023-04-04 16:30:43 +0300

    refactor: Header changed to index.jsx

--- week-2/src/components/Header/index.jsx
+++ week-2/src/components/Header/index.jsx
@@ -4,13 +4,7 @@ const Header = () => {
   return (
     <header className="header">
       <h1>todos</h1>
-      <form>
-        <input
-          className="new-todo"
-          placeholder="What needs to be done?"
-          autoFocus
-        />
-      </form>
+      <Form />
     </header>
   );
 };
