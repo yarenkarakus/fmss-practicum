84ff91d0a96e5da556a97ea04549f77c5dad79b6
Yaren Karakus <yarenkarakus007@gmail.com>
2023-04-04 16:01:14 +0300

    feat: Content component added

---
+++ week-2/src/components/Content.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div>
+      {" "}
+      <section class="main">
+        <input class="toggle-all" type="checkbox" />
+        <label for="toggle-all">Mark all as complete</label>
+
+        <ul class="todo-list">
+          <li class="completed">
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>Learn JavaScript</label>
+              <button class="destroy"></button>
+            </div>
+          </li>
+          <li>
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>Learn React</label>
+              <button class="destroy"></button>
+            </div>
+          </li>
+          <li>
+            <div class="view">
+              <input class="toggle" type="checkbox" />
+              <label>Have a life!</label>
+              <button class="destroy"></button>
+            </div>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default Content;
