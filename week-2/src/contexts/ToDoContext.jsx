0f70eec2e2be81c33d444aa1a4f559cb4c789eb2
Yaren Karakus <yarenkarakus007@gmail.com>
2023-04-05 12:34:28 +0300

    feat: ToDoContext added

---
+++ week-2/src/contexts/ToDoContext.jsx
@@ -0,0 +1,37 @@
+import React, { createContext, useContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+const ToDoContext = createContext();
+
+export const ToDoProvider = ({ children }) => {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Learn React",
+      completed: true,
+    },
+  ]);
+
+  const addTodo = (text) =>
+    setTodos((prev) => [...prev, { id: uuidv4(), completed: false, text }]);
+
+  const values = {
+    todos,
+    setTodos,
+    addTodo,
+  };
+
+  return <ToDoContext.Provider value={values}>{children}</ToDoContext.Provider>;
+};
+
+export const useTodo = () => {
+  const context = useContext(ToDoContext);
+
+  if (context === undefined) {
+    return new Error(
+      "useTodo hook must be called inside ToDoProvider component"
+    );
+  }
+
+  return context;
+};
