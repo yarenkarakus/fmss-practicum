a614571231791e38a36853cdf7ac461709b81471
Yaren Karakus <yarenkarakus007@gmail.com>
2023-04-05 14:29:58 +0300

    feat: completed change method

--- week-2/src/contexts/ToDoContext.jsx
+++ week-2/src/contexts/ToDoContext.jsx
@@ -15,10 +15,21 @@ export const ToDoProvider = ({ children }) => {
   const addTodo = (text) =>
     setTodos((prev) => [...prev, { id: uuidv4(), completed: false, text }]);
 
+  const toggleTodo = (id) => {
+    const cloned_todos = [...todos];
+
+    const itemIndex = cloned_todos.findIndex((todo) => todo.id === id);
+    const item = todos[itemIndex];
+    item.completed = !item.completed;
+
+    setTodos(cloned_todos);
+  };
+
   const values = {
     todos,
     setTodos,
     addTodo,
+    toggleTodo,
   };
 
   return <ToDoContext.Provider value={values}>{children}</ToDoContext.Provider>;
