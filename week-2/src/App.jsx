654f745c795c9ad236a480ccab8940af8c795522
Yaren Karakus <yarenkarakus007@gmail.com>
2023-04-05 14:33:02 +0300

    feat: components and provider added

--- week-2/src/App.jsx
+++ week-2/src/App.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header/index";
+import Content from "./components/Content/Content";
+import Footer from "./components/Footer";
+import { ToDoProvider } from "./contexts/ToDoContext";
 
 function App() {
-  return <div>test</div>;
+  return (
+    <ToDoProvider>
+      <section className="todoapp">
+        <Header />
+        <Content />
+      </section>
+      <Footer />
+    </ToDoProvider>
+  );
 }
 
 export default App;
